c62f1200f756e2292690985ada694254d9499995
Bryan Hodgins <bhodgins89@gmail.com>
2022-03-01 21:50:16 -0500

    Add files via upload

--- database.py
+++ database.py
@@ -67,27 +67,27 @@ class Database: #This is the applications main class. Everything is contained he
         else:
             print("Entry contains unacceptable character, not added.")
         
-    def filter_Entry(self): #function to check that the database entry received from the tkinter UI is valid before pickling
+    def filter_Entry(self, entry): #function to check that the database entry received from the tkinter UI is valid before pickling
         p = re.compile('[a-zA-Z]+ [a-zA-Z]+')  #regex to check that name only consists of one space and alphabetical characters
-        m = p.match(self.entryInfo['Name'])
-        if !m:
+        m = p.match(entry.entryInfo['Name'])
+        if not m:
                 print("invalid name entered")
                 return False
         
         p = re.compile('[a-zA-Z. /-]+') #regex to check position for alphabetical characters and a couple of other possible characters
-        m = p.match(self.entryInfo['Position'])
-        if !m:
+        m = p.match(entry.entryInfo['Position'])
+        if not m:
                 print("invalid position entered")
                 return False
         
         p = re.compile('\d\d\d-\d\d-\d\d\d\d')  #regex to check for SSN match
-        m = p.match(self.entryinfo['SSN'])
-        if !m:
+        m = p.match(entry.entryInfo['SSN'])
+        if not m:
                 print("Invalid SSN entered")
                 return False
         p = re.compile('[0-9]{1,6} \w+ \w*') #regex to check address format
-        m = p.match(self.entryInfo['Address'])
-        if !m:
+        m = p.match(entry.entryInfo['Address'])
+        if not m:
                 print("Invalid address entered")
                 return False
         
@@ -95,20 +95,20 @@ class Database: #This is the applications main class. Everything is contained he
         #long regex. could look at how to split this off, but did not want to mess with it for now
         #this is taken from https://stackabuse.com/python-validate-email-address-with-regular-expressions-regex/
         p = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
-        m = p.match(self.entryInfo['Email'])
-        if !m:
+        m = p.match(entry.entryInfo['Email'])
+        if not m:
                 print("invalid email address entered")
                 return False
         
         p = re.compile('\d\d\d-\d\d\d-\d\d\d\d') #regex for phone number validation
-        m = p.match(self.entryInfo['Phone'])
-        if !m:
+        m = p.match(entry.entryInfo['Phone'])
+        if not m:
                 print("invalid phone number entered")
                 return False
         
         p = re.compile('[a-zA-Z ]+') #regex for skill. makes sure only alphabetical characters and spaces allowed
-        m = p.match(self.entryInfo['Skill'])
-        if !m:
+        m = p.match(entry.entryInfo['Skill'])
+        if not m:
                 print("invalid skill entered")
                 return False
         return True  #if all cases pass
