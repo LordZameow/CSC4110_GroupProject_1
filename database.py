99bcb3694e08193b02dac21fdcd07d5406a4e480
LordZameow <56852089+LordZameow@users.noreply.github.com>
2022-03-01 21:56:53 -0500

    update
    image + tkinter

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-
+import tkinter as tk
 from tkinter import *
 import pickle #Loads database into a file
 import re
@@ -60,34 +60,34 @@ class Database: #This is the applications main class. Everything is contained he
     def add_EntryFilled(self,name, position, SSN, Address, email, phone, skill):
         newEntry = Entry(name, position, SSN, Address, email, phone, skill)
 
-        if self.filter_Entry(newEntry) == True:
-            self.storage.append(newEntry)
+        #if self.filter_Entry(newEntry) == True:
+        self.storage.append(newEntry)
             #print("Entry added.")
             
-        else:
-            print("Entry contains unacceptable character, not added.")
+       # else:
+        #    print("Entry contains unacceptable character, not added.")
         
     def filter_Entry(self): #function to check that the database entry received from the tkinter UI is valid before pickling
         p = re.compile('[a-zA-Z]+ [a-zA-Z]+')  #regex to check that name only consists of one space and alphabetical characters
         m = p.match(self.entryInfo['Name'])
-        if !m:
+        if not m:
                 print("invalid name entered")
                 return False
         
         p = re.compile('[a-zA-Z. /-]+') #regex to check position for alphabetical characters and a couple of other possible characters
         m = p.match(self.entryInfo['Position'])
-        if !m:
+        if not m:
                 print("invalid position entered")
                 return False
         
         p = re.compile('\d\d\d-\d\d-\d\d\d\d')  #regex to check for SSN match
         m = p.match(self.entryinfo['SSN'])
-        if !m:
+        if not m:
                 print("Invalid SSN entered")
                 return False
         p = re.compile('[0-9]{1,6} \w+ \w*') #regex to check address format
         m = p.match(self.entryInfo['Address'])
-        if !m:
+        if not m:
                 print("Invalid address entered")
                 return False
         
@@ -96,19 +96,19 @@ class Database: #This is the applications main class. Everything is contained he
         #this is taken from https://stackabuse.com/python-validate-email-address-with-regular-expressions-regex/
         p = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
         m = p.match(self.entryInfo['Email'])
-        if !m:
+        if not m:
                 print("invalid email address entered")
                 return False
         
         p = re.compile('\d\d\d-\d\d\d-\d\d\d\d') #regex for phone number validation
         m = p.match(self.entryInfo['Phone'])
-        if !m:
+        if not m:
                 print("invalid phone number entered")
                 return False
         
         p = re.compile('[a-zA-Z ]+') #regex for skill. makes sure only alphabetical characters and spaces allowed
         m = p.match(self.entryInfo['Skill'])
-        if !m:
+        if not m:
                 print("invalid skill entered")
                 return False
         return True  #if all cases pass
@@ -145,15 +145,40 @@ class Database: #This is the applications main class. Everything is contained he
             x.print()
 
   
+#tkinter main loop
+def tkmain():   
+    #create tkinter window 
+    root = Tk()
 
+    #open window dimention
+    root.geometry('350x500')
+
+    #add data button
+    adb = Button(root, text = 'Add Data', bd = '5').place(x=100,y=100)
+
+    #add query data button
+    qdb = Button(root, text = 'Query Data', bd = '5').place(x=100,y=200)
+
+    #add import data button
+    ipb = Button(root, text = 'Import Data', bd = '5').place(x=100,y=150)
+
+    #add reverse data button
+    rdb = Button(root, text = 'Reverse Data', bd = '5').place(x=100,y=250)
+
+    #add Archive/upload data button
+    udb = Button(root, text = 'Archive/Upload Data', bd = '5').place(x=100,y=300)
+
+
+    tk.mainloop()
 
 def main():
     base = Database()
     base.import_Txt()
-
+    tkmain()
     base.search_Entry("Skill","Torture")
 
 
 
 if __name__ == "__main__":
     main()
+
